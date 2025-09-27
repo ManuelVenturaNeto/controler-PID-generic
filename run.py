d5e785a6fb83175bbc281802bdb3ff0e1b8e5ed4
Manuel Ventura <manuel.ventura@onfly.com.br>
2025-09-26 21:12:38 -0300

    UI ruim

--- run.py
+++ run.py
@@ -3,16 +3,16 @@ from generic_controler import GenericControler
 
 if __name__ == "__main__":
     controler_definitions = {
-        "kp": 10,
+        "kp": 0.5,
         "ki": 0.5,
         "kd": 0.1,
-        "K": 100,                    # limite superior (0..100)
+        "K": 1500,                    # limite superior (0..100)
         "modo": "automatico",       # "automatico" | "manual"
         "acao": "direta",           # "direta" | "reversa"
         "sp": 50.0,                 # setpoint
         "ts": 1,                    # período de amostragem (s)
         "pv": 0,
-        "anti_reset_windap": 1000
+        "anti_reset_windap": 15000
     }
 
     GenericControler(controler_definitions).main()
